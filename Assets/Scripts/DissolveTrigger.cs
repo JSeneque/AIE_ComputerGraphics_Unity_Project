514095b9dba4eda3007be758614fddd0c0f8c482
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2019-07-05 23:09:40 +0930

    Trigger dissolve by keydown

--- Assets/Scripts/DissolveTrigger.cs
+++ Assets/Scripts/DissolveTrigger.cs
@@ -6,12 +6,72 @@ public class DissolveTrigger : MonoBehaviour
 {
     // store the mesh renderer
     public SkinnedMeshRenderer meshRenderer;
-     
+    public string shaderDissolveValueKey = "Vector1_5FD04FB2";
+    public float startDissolve = 0f;
+    public float reformStart = 1f;
+    private float dissolveRate = 0.01f;
+    private bool readyToDissolve = true;
+
+
 
     private void Update()
     {
+        // press d and start the dissolving
         if (Input.GetKeyDown(KeyCode.D))
-            meshRenderer.material.SetFloat("Vector1_19BE8542", Time.time);
+        {
+            if (readyToDissolve)
+                InvokeDissolve();
+        }
+           
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!readyToDissolve)
+                InvokeReform();
+        }
+            
+    }
+
+    private void InvokeDissolve ()
+    {
+        // repeating call the Dissolve function at a dissolve rate seconds
+        startDissolve = 1;
+        readyToDissolve = false;
+        InvokeRepeating("Dissolve", 0, dissolveRate);
+    }
+
+    private void InvokeReform()
+    {
+        // repeating call the Reform function at a dissolve rate seconds
+        startDissolve = 0.001f;
+        readyToDissolve = true;
+        InvokeRepeating("Reform", 0, dissolveRate);
+    }
+
+    private void Dissolve()
+    {
+        // if in the middle of reforming, cancel it
+        CancelInvoke("Reform");
+
+        meshRenderer.material.SetFloat(shaderDissolveValueKey, startDissolve -= dissolveRate);
+        if (meshRenderer.material.GetFloat(shaderDissolveValueKey) <= 0.01)
+        {
+            readyToDissolve = false;
+            CancelInvoke("Dissolve");
+        }
+    }
+
+    private void Reform()
+    {
+        // if in the middle of dissolving, cancel it
+        CancelInvoke("Dissolve");
+
+        meshRenderer.material.SetFloat(shaderDissolveValueKey, startDissolve += dissolveRate);
+        if (meshRenderer.material.GetFloat(shaderDissolveValueKey) >= 0.95)
+        {
+            readyToDissolve = true;
+            CancelInvoke("Reform");
+        }
     }
 
 }
