5b895d0b92e2616b5cf4c4a8a9a7d312479dbe40
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2019-07-06 18:22:35 +0930

    Press S to activate forcefield

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    private bool isOn = false;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            isOn = !isOn;
+            anim.SetBool("isOn", isOn);
+        }
+    }
+}
