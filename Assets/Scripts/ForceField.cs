46d97ebc714293fcb45e7f67ff3fb73d12a013b5
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2019-07-08 21:06:23 +0930

    Controller added to player

--- Assets/Scripts/ForceField.cs
+++ Assets/Scripts/ForceField.cs
@@ -15,7 +15,7 @@ public class ForceField : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
+        if(Input.GetKeyDown(KeyCode.F))
         {
             isOn = !isOn;
             anim.SetBool("isOn", isOn);
