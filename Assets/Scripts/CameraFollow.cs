386b458a9bc0d3ae9a35af31dbd233e8b241ad8d
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2019-07-10 22:32:31 +0930

    Add controller to player and camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // always comes last, we want to make sure the camera moves after the player moves
+    void LateUpdate()
+    {
+        // move the camera if they are not in the same position towards the player's position
+        if (transform.position != target.position)
+        {
+            Vector3 newPosition = new Vector3(target.position.x, transform.position.y, target.position.z+3.5f);
+            transform.position = Vector3.Lerp(transform.position, newPosition, smoothing);
+        }
+    }
+}
